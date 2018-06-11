54193a1870d38fd56db6f498b9206e8ba855d1cf
Vitold S <vit1251@gmail.com>
2018-06-11 17:43:33 +0300

    Add Rennder class interface

--- chart.go
+++ chart.go
@@ -2,15 +2,25 @@ package chart
 
 import "github.com/fogleman/gg"
 
+type AxisX struct {
+	width int32
+	height int32
+}
+
+type AxisY struct {
+	width int32
+	height int32
+}
+
 type Chart struct {
-	Width int32
-	Height int32
+	width int32
+	height int32
 }
 
 func New(width int32, height int32) (*Chart) {
 	c := &Chart{
-		Width: width,
-		Height: height,
+		width: width,
+		height: height,
 	}
 	return c
 }
@@ -18,10 +28,27 @@ func New(width int32, height int32) (*Chart) {
 func (c *Chart) RegisterInterval(y int32, start_x int32, stop_y int32) {
 }
 
+func (c *Chart) RenderAxes(int dc, a_x *AxisX, a_y *AxisY) {
+	axis_x = &AxisX{width, height}
+	axis_y = &AxisY{width, height}
+}
+
+func (c *Chart) RenderValues(int dc) {
+//	dc.SetRGB(0, 0, 0)
+//	dc.Fill()
+}
+
 func (c *Chart) Render(name string) {
-	dc := gg.NewContext(1000, 1000)
-	dc.DrawCircle(500, 500, 400)
-	dc.SetRGB(0, 0, 0)
-	dc.Fill()
+
+	/* Create new drawing canvas */
+	dc := gg.NewContext(c.width, c.height)
+
+	/* Create and draw axis */
+	c.RenderAxes(dc)
+
+	/* Draw values */
+	c.RenderValues(dc)
+
+	/* Store chart */
 	dc.SavePNG(name)
 }
