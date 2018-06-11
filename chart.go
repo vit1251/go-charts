54b7ca7e8aef81e09f2c1fc33ce863097d635d0a
Vitold S <vit1251@gmail.com>
2018-06-11 18:22:09 +0300

    Porting some source code

--- chart.go
+++ chart.go
@@ -2,19 +2,56 @@ package chart
 
 import "github.com/fogleman/gg"
 
+type Padding struct {
+	Left int /* Default 16 */
+	Right int /* Default 16 */
+	Top int /* Default 16 */
+	Bottom int /* Default 16 */
+}
+
 type AxisX struct {
-	width int
-	height int
+    width int
+    height int
+    step int /* Default 8 */
+    size int /* Default 8 */
+    marker int /* Default 4 */
+    markerSize /* Default 16 */
+    padding Padding
+}
+
+func NewAxisX() (*AxisX) {
+
+	/* Create new axis instance */
+	a_x := &AxisX{}
+
+	/* Setup members */
+	a_x.step = 8
+	a_x.size = 8
+	a_x.marker = 4
+	a_x.markerSize = 16
+
+	/* Setup padding */
+	a_x.padding.Left = 32
+	a_x.padding.Right = 8
+        a_x.padding.Ttop = 8
+        a_x.padding.Bottom = 32
+
+	return a_x
 }
 
 type AxisY struct {
-	width int
-	height int
+}
+
+type Interval struct {
+	Y int
+	StartX int
+	StopX int
 }
 
 type Chart struct {
 	width int
 	height int
+	intervals Interval[]
 }
 
 func New(width int, height int) (*Chart) {
@@ -25,15 +62,61 @@ func New(width int, height int) (*Chart) {
 	return c
 }
 
-func (c *Chart) RegisterInterval(y int, start_x int, stop_x int) {
+func (c *Chart) RegisterInterval(y int, startX int, stopX int) {
+	interval := Interval{
+		Y: y,
+		StartX: startX,
+		StopX: stopX,
+	}
+	c.intervals = append(c.intervals, interval)
+}
+
+func (c *Chart) RenderValues(dc *gg.Context) {
+
+	dc.SetRGB(0, 0, 0)
+	dc.Fill()
+
+	/* Draw values */
+	for _, i := range c.intervals {
+//        start = (axis_x.grid_start_x + axis_x.step * value_start_x, axis_x.grid_stop_y - axis_x.step * value_y)
+//        stop = (axis_x.grid_start_x + axis_x.step * value_stop_x, axis_x.grid_stop_y - axis_x.step * value_y)
+//        draw.line([start, stop], fill=color, width=3)
+	}
+}
+
+func (c *Chart) RendexGridX(dc *gg.Context) {
+
+    /* Draw grid on X */
+//    for x in range(axis_x.grid_start_x, axis_x.grid_stop_x, axis_x.step * axis_x.marker):
+//        start = (x, 50)
+//        stop  = (x, axis_x.start_y)
+//        draw.line([start, stop], fill=axis_x.grid_color, width=0)
+
 }
 
 func (c *Chart) RenderAxesX(dc *gg.Context) {
-	axis_x := &AxisX{width, height}
+
+	a_x := NewAxisX()
+
+        /* Draw baseline */
+//        draw.line([(axis_x.start_x, axis_x.start_y), (axis_x.stop_x, axis_x.stop_y)], fill=axis_x.color, width=0)
+
+        /* Draw smaller scale */
+//        for x in range(axis_x.grid_start_x, axis_x.grid_stop_x, axis_x.step):
+//            start = (x, axis_x.start_y)
+//            stop  = (x, axis_x.start_y + axis_x.size)
+//            draw.line([start, stop], fill=axis_x.color, width=0)
+
+        /* Draw medium scale */
+//        for x in range(axis_x.grid_start_x, axis_x.grid_stop_x, axis_x.step * axis_x.marker):
+//            start = (x, axis_x.start_y)
+//            stop  = (x, axis_x.start_y + axis_x.marker_size)
+//            draw.line([start, stop], fill=axis_x.color, width=0)
+
 }
 
 func (c *Chart) RenderAxesY(dc *gg.Context) {
-	axis_y := &AxisY{width, height}
+	axis_y := &AxisY{c.width, c.height}
 }
 
 func (c *Chart) RenderAxes(dc *gg.Context) {
@@ -46,10 +129,6 @@ func (c *Chart) RenderAxes(dc *gg.Context) {
 
 }
 
-func (c *Chart) RenderValues(dc *gg.Context) {
-	dc.SetRGB(0, 0, 0)
-	dc.Fill()
-}
 
 func (c *Chart) Render(name string) {
 
